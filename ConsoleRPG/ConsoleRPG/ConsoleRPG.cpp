a48575baf782e9566be56ef99d49094c385cdafa
Kamil Skrzypkowski <sk.kamil6@wp.pl>
2018-11-28 21:36:51 +0100

    So I finally (did something) added any action to game :)

--- ConsoleRPG/ConsoleRPG/ConsoleRPG.cpp
+++ ConsoleRPG/ConsoleRPG/ConsoleRPG.cpp
@@ -3,23 +3,102 @@
 #include <Windows.h>
 #include <string>
 
+//Stcructure shuldn't be in main??????
 struct NPC
 {
 	std::string name;
-	int height, width, weight, race, age, positionX, posiotionY;
+	int height, width, weight, race, age, positionX, positionY;
 
 };
 
-void Game_loop() {
+//testing function (my favourite)
+void testing() {
 	system("cls");
-	struct NPC player;
 	
-	//something to think about
-	//bool map[][];
-
+	
+	
 }
 
-//Here should be main loop of the game, but insted I created butload of code typing words.
+
+//Working pattern for creating 23x79 plane to walk on.
+void map_creator(bool map_to_walk_on[23][79], char map_drawing[23][79]) {
+	for (int y = 0; y < 23; y++) {
+		for (int x = 0; x < 79; x++) {
+			if (y == 0 || y == 22 || x == 0 || x == 78)
+			{
+				map_to_walk_on[y][x] = false;
+				map_drawing[y][x] = (char)219;
+			}
+			else{
+				map_to_walk_on[y][x] = true;
+				map_drawing[y][x] = ' ';
+			}
+		}
+	}
+}
+
+//Function that is drawing table
+void map_drawer(bool map_to_walk_on[23][79], char map_drawing[23][79]) {
+	system("cls");
+
+	for (int y = 0; y < 23; y++) {
+		for (int x = 0; x < 79; x++) {
+			std::cout << map_drawing[y][x];
+		}
+		std::cout << std::endl;
+	}
+}
+
+//New main loop
+void Game_loop(bool map_to_walk_on[23][79], char map_drawing[23][79]) {
+	system("cls");
+
+	//Prepering variables, structures and map
+	std::string what_you_are_going_to_do;
+	bool exit = true;
+	struct NPC player;
+	player.positionX = 1;
+	player.positionY = 1;
+	map_creator(map_to_walk_on, map_drawing);
+
+	do{
+		//Placing player on his position
+		map_drawing[player.positionY][player.positionX] = (char)1;
+		//Drawing map
+		map_drawer(map_to_walk_on, map_drawing);
+		//What player is going to do
+		std::cout << "What do you want to do?" << std::endl;
+		std::cin >> what_you_are_going_to_do;
+		//Replacing old player location
+		map_drawing[player.positionY][player.positionX] = ' ';
+
+		//walking
+		if (what_you_are_going_to_do == "up" && map_to_walk_on[player.positionY - 1][player.positionX] == true)
+		{
+			player.positionY--;
+		}
+		else if (what_you_are_going_to_do == "down" && map_to_walk_on[player.positionY + 1][player.positionX] == true)
+		{
+			player.positionY++;
+		}
+		else if (what_you_are_going_to_do == "left" && map_to_walk_on[player.positionY][player.positionX - 1] == true)
+		{
+			player.positionX--;
+		}
+		else if (what_you_are_going_to_do == "right" && map_to_walk_on[player.positionY][player.positionX + 1] == true)
+		{
+			player.positionX++;
+		}
+
+		//exiting
+		if (what_you_are_going_to_do == "exit")
+		{
+			exit = false;
+		}
+	} while (exit);
+}
+
+//Here should be main loop of the game, but insted I created butload of code typing words. (for later use mabye)
 //*************************************************************************************************************************************************************************
 void game_start() {
 	system("cls");
@@ -51,9 +130,12 @@ void game_start() {
 }
 //*************************************************************************************************************************************************************************
 
-int main()
+void main()
 {
 	int choise;
+	bool map_to_walk_on[23][79];
+	char map_drawing[23][79];
+
     std::cout << "\n\n\n\n\n\n\n\n\n\n\n                            Welcome in ConsoleRPG"; 
 	std::cin.get(); system("cls");
 	std::cout << "1.New Game \n2.Load Game \n3.Options \n4.Exit";
@@ -61,13 +143,15 @@ int main()
 
 	switch (choise)
 	{
-	case 1: 
+	case 1: Game_loop(map_to_walk_on, map_drawing);
 		break;
 	case 2: std::cout << "Still working on it" <<std::endl;
 		break;
 	case 3: std::cout << "Still working on it" << std::endl;
 		break;
 	case 4: std::cout << "Bye." << std::endl;
+		break;
+	case 666: testing();
 		break;
 	default: std::cout << "Beeep.... "; Sleep(900); std::cout << "beep...."; Sleep(900); std::cout <<" boop...." << std::endl; Sleep(2000); std::cout << "Shuting down" << std::endl;
 		break;
